7038d16a666e40eacbe110f636de3737cfe838ae
inguelee <55696282+inguelee@users.noreply.github.com>
2020-04-06 15:42:18 +0900

    Revert "Jaehyuk"

--- source.cpp
+++ source.cpp
@@ -1,13 +1,4 @@
 #include <stdio.h>
-int jaehyuk(float);
-int main()
-{
-	jaehyuk(3000);
-	return 0;
-}
-void jaehyuk(float meter)
-{
-	printf("%.2f", meter/1000);	
 
 void kimeunjee();
 int yearCheck(int year);
