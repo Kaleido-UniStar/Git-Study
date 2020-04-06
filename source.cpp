a31c8129ef660f9f3939d66d554a5906f372e8f8
eunjee33 <eunjee3333@naver.com>
2020-04-06 15:04:47 +0900

    yoon

---
+++ source.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void kimeunjee();
+int yearCheck(int year);
+
+int main() {
+	kimeunjee();
+	return 0;
+}
+
+void kimeunjee()
+{
+	int year = 2020;
+	int check = yearCheck(year);
+	if (check == 0) {
+		printf("평년(Common year)입니다\n");
+	}
+	else {
+		printf("윤년(Leap year)입니다\n");
+	}
+}
+int yearCheck(int year)
+{
+	if ((year % 4) != 0) {
+		return 0;
+	}
+	else if ((year % 100) != 0) {
+		return 1;
+	}
+	else if ((year % 400) != 0) {
+		return 0;
+	}
+	else {
+		return 1;
+	}
+}
